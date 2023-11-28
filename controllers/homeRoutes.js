0c028b98b7f7d3a055fe1034745cf829ddbbdedb
Tiffany Perez <pereztb02@gmail.com>
2023-11-28 17:45:23 -0500

    got all routes working and views rendering

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,25 +4,22 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all post and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    if (req.session.logged_in) {
+      const userPostsData = await Post.findAll({
+        where: { user_id: req.session.user_id },
+      });
 
-    // Serialize data so the template can read it
-    const post = postData.map((post) => post.get({ plain: true }));
+      const userPosts = userPostsData.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      post, 
-      logged_in: req.session.logged_in 
-    });
+      res.render('homepage', {
+        userPosts,
+        logged_in: true,
+      });
+    } else {
+      res.render('homepage', { logged_in: false });
+    }
   } catch (err) {
+    console.error(err);
     res.status(500).json(err);
   }
 });
@@ -42,7 +39,7 @@ router.get('/post/:id', async (req, res) => {
 
     res.render('post', {
       ...post,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -62,7 +59,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
     res.render('profile', {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -79,4 +76,13 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile'); // Redirect to the user's profile page
+    return;
+  }
+
+  res.render('signup');
+});
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector("#password-login").value.trim();
 
   if (email && password) {
-    const response = await fetch("/api/users/signin", {
+    const response = await fetch("/api/users/login", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
@@ -42,7 +42,7 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector(".signin-form")
+  .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
 
 document
