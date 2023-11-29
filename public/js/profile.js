4efe244c7ecada719d473fdabac46de3471a2d9b
Tiffany Perez <pereztb02@gmail.com>
2023-11-28 19:51:49 -0500

    troubleshooting

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,50 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#post-name').value.trim();
-    const post_content = document.querySelector('#post-content').value.trim();
-    // const postUserName = document.querySelector('#post-username').value.trim();
-  
-    if (name && post_content) {
-      const response = await fetch(`/api/post`, {
-        method: 'POST',
-        body: JSON.stringify({ name, post_content }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create post');
-      }
+  event.preventDefault();
+
+  const name = document.querySelector('#post-name').value.trim();
+  const post_content = document.querySelector('#post-content').value.trim();
+  // const postUserName = document.querySelector('#post-username').value.trim();
+console.log('Name:', name);
+console.log('Post content:', post_content);
+
+  if (name && post_content) {
+    const response = await fetch(`/api/post`, {
+      method: 'POST',
+      body: JSON.stringify({ name, post_content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      console.log('redirecting to profile page');
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create post');
     }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/post/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete post');
-      }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/post/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete post');
     }
-  };
-  
-  document
-    .querySelector('.new-post-form')
-    .addEventListener('submit', newFormHandler);
-  
-  document
-    .querySelector('.post-list')
-    .addEventListener('click', delButtonHandler);
+  }
+};
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.post-list')
+  .addEventListener('click', delButtonHandler);
